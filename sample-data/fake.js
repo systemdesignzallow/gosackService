201443c98733b820d2ecbe2849eb0b76e184dd28
Patrick Gosack <gosack@gmail.com>
2019-07-27 15:12:21 -0700

    update to fake removed a bug with the date

--- sample-data/fake.js
+++ sample-data/fake.js
@@ -38,8 +38,32 @@ const csvStringifier = createCsvStringifier({
     fieldDelimiter: ';'
 });
 
+function getDateTime() {
+    var date = new Date();
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+}
+
 let i = 1e7;
-console.log(Date.now());
+
+console.log(getDateTime());
+
 write();
 
 function write() {
@@ -106,15 +130,15 @@ function write() {
         
         i--;
         
-        if(i % 1e4 === 0){
+        if(i % 1e5 === 0){
             console.log(i);
-        } else if (i % 1e5 === 0) {
-            console.clear();
-        }
+        } 
+        
     } while (i > 0 && ok);
 
     if (i > 0) {
         writeStream.once('drain', write);
     }
-    console.log(Date.now());
 }
+
+console.log(Date.now());
