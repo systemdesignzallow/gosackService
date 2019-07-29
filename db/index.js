a2b122cd6848621bcb34f0c922b165e1479c70db
Patrick Gosack <gosack@gmail.com>
2019-07-29 11:59:15 -0700

    reformat for readability

--- db/index.js
+++ db/index.js
@@ -1,6 +1,11 @@
 const mariadb = require('mariadb');
 
 // sock : /run/mysqld/mysqld.sock --I found this by stopping the mariaDb service and attempting to run the cli
-const homes = mariadb.createPool({socketpath: '/run/mysqld/mysqld.sock', user: 'patrick', password:'ekauq', connectionLimit: 5, database:'gosackService'});
+const homes = mariadb.createPool({
+    socketpath: '/run/mysqld/mysqld.sock',
+    user: 'patrick',
+    password:'ekauq',
+    database:'gosackService'
+});
 
 module.exports = homes;
