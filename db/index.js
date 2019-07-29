6e8bb35830c77d5f3f33751f745be8c1c0a3a15a
Patrick Gosack <gosack@gmail.com>
2019-07-29 11:22:43 -0700

    exports change

--- db/index.js
+++ db/index.js
@@ -3,6 +3,4 @@ const mariadb = require('mariadb');
 // sock : /run/mysqld/mysqld.sock --I found this by stopping the mariaDb service and attempting to run the cli
 const homes = mariadb.createPool({socketpath: '/run/mysqld/mysqld.sock', user: 'patrick', password:'ekauq', connectionLimit: 5, database:'gosackService'});
 
-module.exports = {
-     House: homes
-  };
+module.exports = homes;
