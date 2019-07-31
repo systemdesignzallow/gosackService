90c89921b98308352fcfe75d859588acc4335ed3
Patrick Gosack <gosack@gmail.com>
2019-07-31 13:25:12 -0700

    update babel config with jest

--- server/test.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-let app = express();
-
-app.use(express.static('./public', { index: 'client-test.html' }));
-
-app.listen(9002, () => {
-  console.log('testing on localhost:9002');
-});
