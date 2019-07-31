04e24389da072a1bda356c275027239f4b6467f2
Patrick Gosack <gosack@gmail.com>
2019-07-30 17:52:13 -0700

    file is linted

--- db/index.js
+++ db/index.js
@@ -1,6 +1,17 @@
 const mariadb = require('mariadb');
 
 // sock : /run/mysqld/mysqld.sock --I found this by stopping the mariaDb service and attempting to run the cli
+// const homes = mariadb.createPool({
+//   // change for test/dev/production
+//   // process.env
+//   // set environment variable in npm script
+//   socketpath: '/run/mysqld/mysqld.sock',
+//   user: 'patrick',
+//   password: 'ekauq',
+//   // make special database for test
+//   database: 'gosackService'
+// });
+
 const homes = mariadb.createPool({
   // change for test/dev/production
   // process.env
@@ -9,7 +20,7 @@ const homes = mariadb.createPool({
   user: 'patrick',
   password: 'ekauq',
   // make special database for test
-  database: 'gosackService'
+  database: 'testService'
 });
 
 module.exports = homes;
