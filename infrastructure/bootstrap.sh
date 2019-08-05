9951e46e835310d37d16fd36be7f9f26f6f15828
Patrick Gosack <gosack@gmail.com>
2019-08-05 14:53:20 -0700

    changes for terraform

--- infrastructure/bootstrap.sh
+++ infrastructure/bootstrap.sh
@@ -4,6 +4,7 @@ sudo amazon-linux-extras install docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo yum install git -y
+sudo yum install vim
 
 git clone https://github.com/systemdesignzallow/gosackService
 cd gosackService
